b30168a3ca95eea4b78ffdda89c836e86f92fc6c
Michael Koller <koller_michael@outlook.de>
2026-03-06 22:05:47 +0100

    new odin version

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 mkdir downloads
 
 # download and unpack Odin
-wget https://github.com/odin-lang/Odin/releases/download/dev-2025-12a/odin-linux-amd64-dev-2025-12a.tar.gz
-tar -xf odin-linux-amd64-dev-2025-12a.tar.gz
-rm odin-linux-amd64-dev-2025-12a.tar.gz
+wget https://github.com/odin-lang/Odin/releases/download/dev-2026-03/odin-linux-amd64-dev-2026-03.tar.gz
+tar -xf odin-linux-amd64-dev-2026-03.tar.gz
+rm odin-linux-amd64-dev-2026-03.tar.gz
 # move to downloads
-mv odin-linux-amd64-nightly+2025-12-04 downloads
+mv odin-linux-amd64-nightly+2026-03-03 downloads
